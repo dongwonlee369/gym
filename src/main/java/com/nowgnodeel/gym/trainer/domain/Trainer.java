404608dc239a7c54fa8a36a6aa90611bae212a0e
dongwonlee369 <dongwonlee369@gmail.com>
2024-01-03 14:01:17 +0900

    feat: 트레이너 테이블 추가

--- src/main/java/com/nowgnodeel/gym/trainer/domain/Trainer.java
+++ src/main/java/com/nowgnodeel/gym/trainer/domain/Trainer.java
@@ -1,4 +1,32 @@
 package com.nowgnodeel.gym.trainer.domain;
 
-public class Trainer {
+import com.nowgnodeel.gym.global.domain.BaseTimeEntity;
+import com.nowgnodeel.gym.global.domain.Sex;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Trainer extends BaseTimeEntity {
+
+  @Column(nullable = false)
+  private String nickname;
+
+  @Column(nullable = false)
+  private String birth;
+
+  @Column(nullable = false)
+  private Sex sex;
+
+  @Column(nullable = false)
+  private String phone;
+
+  @Column(nullable = false)
+  private String address;
 }
